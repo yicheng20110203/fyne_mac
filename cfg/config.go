4b9ba8c3d9a433dfbd0626c6b8da6e2bf209a8cb
dyong <dyong@ceibsdigital.com>
2020-12-23 14:14:32 +0800

    fix

--- cfg/config.go
+++ cfg/config.go
@@ -2,11 +2,7 @@ package cfg
 
 const (
     SmsGateway         = "http://dysmsapi.aliyuncs.com/"
-    SmsAccessKeyId     = "LTAI4GGcf27EkcEckKSKPvpe"
-    SmsAccessKeySecret = "pRNcddEWvFjHTn0qWRa0J6o4sLSDVQ"
-    //SmsSignName        = "中欧商业评论"
-    //SmsTplCode         = "SMS_202820483"
-    //SmsTplCodeV2       = "SMS_206735913"
-    //SmsTplCodeV3       = "SMS_207175644"
+    SmsAccessKeyId     = "" // TODO 填写自己的key
+    SmsAccessKeySecret = "" // TODO 填写自己的secret
     EachSendSize       = 10
 )
